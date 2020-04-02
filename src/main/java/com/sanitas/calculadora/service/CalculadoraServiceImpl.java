ff2298e9474358457491d682c35bf77be7a49d42
Robin Arellano <robinarehen@gmail.com>
2020-04-02 14:59:43 +0200

    refactor: 1. se crea el test para [sumar] el cual falla por no estar implementado

---
+++ src/main/java/com/sanitas/calculadora/service/CalculadoraServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sanitas.calculadora.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculadoraServiceImpl {
+
+	public int sumar(int numero1, int numero2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/sanitas/calculadora/service/CalculadoraServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.sanitas.calculadora.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculadoraServiceImplTest {
+
+	@Autowired
+	private CalculadoraServiceImpl calculadoraService;
+
+	@Test
+	public void sumar() {
+
+		int numero1 = 2;
+		int numero2 = 3;
+
+		int resultado = this.calculadoraService.sumar(numero1, numero2);
+
+		int expected = numero1 + numero2;
+
+		assertThat(expected).isEqualTo(resultado);
+	}
+}

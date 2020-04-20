55e07ef31df1137c4a37778c2c9d2c891fdfad63
Robin Arellano <robinarehen@gmail.com>
2020-04-20 14:53:37 +0200

    refactor: se elimina el metodo que no estaba siendo utilizado

--- src/main/java/com/sanitas/calculadora/dto/CalculadoraDto.java
+++ src/main/java/com/sanitas/calculadora/dto/CalculadoraDto.java
@@ -1,7 +1,5 @@
 package com.sanitas.calculadora.dto;
 
-import com.sanitas.calculadora.util.Operadores;
-
 import lombok.Data;
 
 @Data
@@ -10,14 +8,5 @@ public class CalculadoraDto {
 	private String operador;
 	private Integer num1;
 	private Integer num2;
-	
-	public void validOperator() {
-		try {
-			Operadores.valueOf(operador);
-		}catch (Exception e) {
-			// TODO: handle exception
-			throw new IllegalArgumentException("Operador no valido");
-		}
-	}
 
 }

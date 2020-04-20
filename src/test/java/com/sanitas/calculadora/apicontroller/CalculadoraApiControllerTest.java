fe8dc3908ac7fd7f33b0ff11dc2ead02654f7b59
Robin Arellano <robinarehen@gmail.com>
2020-04-20 23:44:09 +0200

    refactor: 1. se implementan las pruebas para el api en el metodo operaciones

--- src/test/java/com/sanitas/calculadora/apicontroller/CalculadoraApiControllerTest.java
+++ src/test/java/com/sanitas/calculadora/apicontroller/CalculadoraApiControllerTest.java
@@ -2,6 +2,7 @@ package com.sanitas.calculadora.apicontroller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sanitas.calculadora.dto.CalculadoraDto;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CalculadoraApiControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
 	private MvcResult mvcResult;
 	
 	private Integer numero1;
@@ -69,5 +76,49 @@ public class CalculadoraApiControllerTest {
 		String resultActual = this.mvcResult.getResponse().getContentAsString();
 
 		assertThat(resultActual).isEqualTo(resultExpected.toString());
+	}
+
+	@Test
+	public void operacionSuma() throws Exception {
+
+		this.postRequest("suma");
+
+		Integer resultExpected = this.numero1 + this.numero2;
+		String resultActual = this.mvcResult.getResponse().getContentAsString();
+
+		assertThat(resultActual).isEqualTo(resultExpected.toString());
+	}
+	
+	@Test
+	public void operacionResta() throws Exception {
+
+		this.postRequest("resta");
+
+		Integer resultExpected = this.numero1 - this.numero2;
+		String resultActual = this.mvcResult.getResponse().getContentAsString();
+
+		assertThat(resultActual).isEqualTo(resultExpected.toString());
+	}
+	
+	private void postRequest(String operacion) throws Exception {
+
+		String url = "/calculadora/operacion";
+
+		CalculadoraDto calculadoraDto = new CalculadoraDto(operacion, this.numero1, this.numero2);
+		
+		this.mvcResult = this.mockMvc.perform(
+					post(url)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(this.mapToJson(calculadoraDto))
+				).andReturn();
+		
+		Integer statusExpected = 200;
+		Integer statusActual = this.mvcResult.getResponse().getStatus();
+		
+		assertThat(statusActual).isEqualTo(statusExpected);
+	}
+	
+	private String mapToJson(Object obj) throws JsonProcessingException {
+		return this.objectMapper.writeValueAsString(obj);
 	}
 }
